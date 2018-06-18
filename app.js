38be85f442118c7ff2038178ec598abac6fc8b2a
fiatjaf <fiatjaf@gmail.com>
2018-06-18 16:44:47 -0300

    hierarchical-interactive way of listing pins.

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const CID = require('cids')
 const render = require('react-dom').render
 const React = require('react')
 const h = require('react-hyperscript')
+const prettyBytes = require('pretty-bytes')
 
 
 const nonDirBlocks = JSON.parse(localStorage.getItem('non-dir-blocks') || '{}')
 const dirBlocks = JSON.parse(sessionStorage.getItem('dir-blocks') || '{}')
-const g = window.g = new graphlib.Graph()
+const g = window.g = new graphlib.Graph({directed: true})
 
 class App extends React.Component {
   constructor (props) {
@@ -20,12 +21,13 @@ class App extends React.Component {
       log: [],
       logIndex: 1,
       npins: null,
-      sources: []
+      sources: [],
+      selected: null
     }
   }
 
   async componentDidMount () {
-    this.log('reading all pinned refs')
+    this.log('reading all pinned refs (this may take a while)')
     let pins = await ipfs.pin.ls()
     this.log(`got ${pins.length} refs`)
 
@@ -38,10 +40,7 @@ class App extends React.Component {
 
     this.log('finished reading refs')
 
-    this.setState(st => {
-      st.sources = g.sources()
-      return st
-    })
+    this.recalc()
   }
 
   log (message) {
@@ -51,6 +50,12 @@ class App extends React.Component {
       st.logIndex++
       return st
     })
+  }
+
+  async recalc () {
+    let sources = g.sources()
+
+    this.setState({sources})
   }
 
   async checkDirectory (ref) {
@@ -105,35 +110,181 @@ class App extends React.Component {
           ])
         ])
       ]),
-      h('#trees', {key: 'sources'}, this.state.sources.map(s =>
-        h(Tree, {root: s})
-      )),
+      h('main', {key: 'main'}, [
+        this.state.selected &&
+          h(Sidebar, {
+            onUnpin: hash => this.unpin(hash),
+            hash: this.state.selected
+          }),
+        h('#tree', {key: 'tree'}, this.state.sources.map(hash =>
+          h(Tree, {
+            hash,
+            onSelect: h => this.select(h)
+          })
+        ))
+      ]),
       h('#log', {key: 'log'}, this.state.log.map(entry =>
         h('p', {key: entry}, entry)
       ))
     ]
   }
+
+  select (hash) {
+    if (hash === this.state.selected) {
+      this.setState({selected: null})
+    }
+
+    this.setState({selected: hash})
+  }
+
+  async unpin (hash) {
+    await ipfs.pin.rm(hash, {recursive: true})
+
+    removeWithOrphanDescendants(hash)
+
+    this.recalc()
+  }
 }
 
-function Tree ({root}) {
-  return (
-    h('.tree', [
-      h('h1', root),
-      h('table', [
-        h('tbody', g.outEdges(root).map(({v, w}) => {
-          let {name, size} = g.edge(v, w)
+class Tree extends React.Component {
+  constructor (props) {
+    super(props)
 
-          return (
-            h('tr', {key: w}, [
-              h('th', name),
-              h('td', size),
-              h('td', [ h(Tree, {root: w}) ])
-            ])
-          )
-        }))
+    this.state = {
+      opened: false
+    }
+
+    this.nChildren = g.outEdges(props.hash).length
+  }
+
+  render () {
+    return (
+      h('.tree', [
+        h('div', [
+          this.nChildren > 0
+            ? h('span.n-children',
+              `(${this.nChildren} ${this.nChildren > 1 ? 'children' : 'child'})`
+            )
+            : h('span.n-children'),
+          this.nChildren > 0 &&
+            h('a.open', {
+              onClick: e => {
+                e.preventDefault()
+                this.setState({opened: !this.state.opened})
+              }
+            }, this.state.opened ? '-' : '+'),
+          h('span.name', this.props.name || ''),
+          ' ',
+          h('span.hash', [
+            h('a', {
+              href: `https://ipfs.io/ipfs/${this.props.hash}`,
+              target: '_blank'
+            }, this.props.hash)
+          ]),
+          ' ',
+          h('span.size', [
+            h('a', {
+              onClick: e => {
+                e.preventDefault()
+                this.props.onSelect(this.props.hash)
+              }
+            }, typeof this.props.size === 'number' ? prettyBytes(this.props.size) : '~')
+          ])
+        ]),
+        this.state.opened
+          ? h('ul', g.outEdges(this.props.hash).map(({v, w}) => {
+            let {size, name} = g.edge(v, w)
+
+            return (
+              h('li', [
+                h(Tree, {
+                  onSelect: hash => this.props.onSelect(hash),
+                  hash: w,
+                  name,
+                  size
+                })
+              ])
+            )
+          }))
+          : null
       ])
-    ])
-  )
+    )
+  }
+}
+
+class Sidebar extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      providers: null
+    }
+  }
+
+  render () {
+    return (
+      h('#sidebar', [
+        h('h1', this.props.hash),
+        h('h2', g.node(this.props.hash) ? 'pinned' : 'not pinned'),
+        h('div', [
+          'Linked',
+          h('table', [
+            h('thead', [
+              h('tr', [
+                h('th', 'on'),
+                h('th', 'as')
+              ])
+            ]),
+            h('tbody', g.inEdges(this.props.hash).map(({v, w}) =>
+              h('tr', [
+                h('td', [
+                  h('a', {href: `https://ipfs.io/ipfs/${v}`, target: '_blank'}, v)
+                ]),
+                h('td', g.edge(v, w).name)
+              ])
+            ))
+          ])
+        ]),
+        h('.providers', [
+          this.state.providers
+            ? [
+              h('p', `${this.state.providers.length} providers.`),
+              h('ul', this.state.providers.map(p =>
+                h('li', p.ID)
+              ))
+            ]
+            : h('button', {
+              onClick: async e => {
+                e.preventDefault()
+
+                let providers = await ipfs.dht.findprovs(this.props.hash)
+                this.setState({providers})
+              }
+            }, 'Find providers')
+        ]),
+        h('.commands', [
+          h('button', {
+            onClick: e => {
+              e.preventDefault()
+
+              this.props.onUnpin()
+            }
+          }, 'Unpin')
+        ])
+      ])
+    )
+  }
+}
+
+function removeWithOrphanDescendants (hash) {
+  // get all children of <hash> and remove those
+  // who will be orphans once we remove <hash>.
+  g.outEdges(hash).forEach(dep => {
+    if (g.inEdges(dep).length === 1) {
+      removeWithOrphanDescendants(dep)
+    }
+  })
+  g.removeNode(hash)
 }
 
 render(h(App), document.body)
